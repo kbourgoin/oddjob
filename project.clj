2b3a66e3965720d7e755fd68579feb5b344f89d0
Keith Bourgoin <keith@parsely.com>
2011-06-14 21:19:04 -0400

    Updating project.clj with versions of clojure in maven central.

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
             :description "Hadoop utilities for MrJob that must run in the JVM"
             :dependencies [[org.clojure/clojure "1.2.1"]
                            [org.clojure/clojure-contrib "1.2.0"]
-                           [org.apache.hadoop/hadoop-streaming "0.18.3"]]
-            :dev-dependencies [[org.apache.hadoop/hadoop-core-with-dependencies "0.18.3"]
+                           [org.apache.hadoop/hadoop-streaming "0.20.203.0"]]
+            :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.203.0"]
                                [swank-clojure "1.2.1"]]
             :main oddjob.core
             :aot :all)
